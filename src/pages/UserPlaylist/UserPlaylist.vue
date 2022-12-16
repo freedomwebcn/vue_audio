b7b436979f599c0a09ef4dcf3122ca84f43a686b
freedomwebcn <freedomweb@yeah.net>
2022-12-16 19:46:37 +0800

    完成歌单获取

--- src/pages/UserPlaylist/UserPlaylist.vue
+++ src/pages/UserPlaylist/UserPlaylist.vue
@@ -4,22 +4,16 @@
       <div class="card-profile_visual">
         <div class="bg" :style="bgStyleObj"></div>
       </div>
-      <div class="card-profile_user-infos">
-        <span class="infos_name comm">jack</span>
-        <span class="infos_last-login-address comm">最后登陆地址:陕西</span>
-        <img :src="profile.avatarUrl" alt="" class="avt_img" />
-      </div>
+      <img :src="profile.avatarUrl" alt="" class="avt_img" />
     </div>
-
     <div class="playlist">
       <div class="playlist-header">
         <span class="create-playlist">创建的歌单</span>
         <span class="collect-playlist">收藏的歌单</span>
       </div>
-
       <div class="playlist-content">
         <ul>
-          <li v-for="item in createPlaylistData" :key="item.id">
+          <li v-for="item in createPlaylistData" :key="item.id" :data-id="item.id">
             <img :src="item.coverImgUrl" alt="" />
             <div>
               <h3>{{ item.name }}</h3>
@@ -39,7 +33,6 @@ const profile = $ref({});
 let uid;
 const createPlaylistData = $ref([]);
 const collectPlaylistData = $ref([]);
-
 const bgStyleObj = $ref();
 
 async function getUserAccountInfo() {
@@ -66,7 +59,7 @@ function formatData(data) {
       createPlaylistData.push({ name, trackCount, coverImgUrl, playCount, id });
     } else {
       const { name, trackCount, coverImgUrl, playCount, id } = playlist;
-      collectPlaylistData.push({ name, trackCount, coverImgUrl, playCount, id });
+      collectPlaylistData.push({ name, trackCount, nickname: playlist.creator.nickname, coverImgUrl, playCount, id });
     }
   });
 }
@@ -79,6 +72,7 @@ function formatData(data) {
   position: relative;
   overflow: hidden;
   overflow-y: scroll;
+  border-radius: 5px;
   box-shadow: 0px 8px 28px -9px rgb(0 0 0 / 45%);
 }
 
@@ -98,9 +92,8 @@ function formatData(data) {
   height: 186px;
   overflow: hidden;
   position: relative;
-
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
+  /* border-top-left-radius: 10px;
+  border-top-right-radius: 10px; */
 }
 .card-profile_visual .bg {
   width: 100%;
@@ -110,52 +103,32 @@ function formatData(data) {
   background: url(./01.png);
 }
 
-.card-profile_user-infos {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  position: absolute;
-  z-index: 3;
-
-  margin: auto;
-  top: calc(60% - 26px);
-  color: #fff;
-  text-align: center;
-}
-
-.card-profile_user-infos .comm {
-  flex: 100%;
-}
-
-.card-profile_user-infos .infos_name {
-  padding-bottom: 9px;
-}
-
-.infos_last-login-address {
-  font-size: 14px;
-  padding-bottom: 12px;
-}
-
-.card-profile_user-infos .avt_img {
+.avt_img {
   width: 64px;
   height: 64px;
+  position: absolute;
+  bottom: -32px;
   border-radius: 50%;
 }
 
 .playlist {
-  padding: 56px 10px 0px 10px;
+  padding: 43px 10px 0px 10px;
 }
 
 .playlist-header {
   display: flex;
   justify-content: space-between;
   padding: 0 15px;
+  font-size: 14px;
 }
 
 .playlist-header .create-playlist {
   color: rgb(253, 57, 58);
 }
 
+.playlist-header .collect-playlist {
+  color: rgb(50, 50, 50);
+}
 .playlist-content {
   padding: 15px 0 0 0;
   margin-bottom: 15px;
@@ -172,7 +145,7 @@ function formatData(data) {
 .playlist-content li img {
   width: 50px;
   height: 50px;
-  margin-right: 15px;
+  margin-right: 10px;
   border-radius: 5px;
   object-fit: cover;
 }
@@ -185,7 +158,7 @@ function formatData(data) {
 }
 .playlist-content li h3 {
   font-weight: normal;
-  font-size: 13px;
+  font-size: 15px;
   flex: 100%;
   white-space: nowrap;
   max-width: 216px;
